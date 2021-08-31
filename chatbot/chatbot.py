ec29da2f8fb41926d570770017aecc2b650893d5
Francis Guevara <69579186+franciswguevara@users.noreply.github.com>
2021-08-31 08:55:22 +0000

    Set Webhook Trial

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -26,33 +26,33 @@ bot_configuration = BotConfiguration(
 )
 
 viber = Api(bot_configuration)
-#viber.set_webhook('https://selecta-b2b-viber.herokuapp.com/')
+viber.set_webhook('https://selecta-b2b-viber.herokuapp.com/')
 print('started')
 
-@app.route('/', methods = ['POST'])
-def incoming():
+# @app.route('/', methods = ['POST'])
+# def incoming():
 
-  logger.debug("received request. post data: {0}".format(request.get_data()))
-    # every viber message is signed, you can verify the signature using this method
-  if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
-      return Response(status=403)
+#   logger.debug("received request. post data: {0}".format(request.get_data()))
+#     # every viber message is signed, you can verify the signature using this method
+#   if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+#       return Response(status=403)
 
-  #Parse the request object
-  viber_request = viber.parse_request(request.get_data())
+#   #Parse the request object
+#   viber_request = viber.parse_request(request.get_data())
 
-  if isinstance(viber_request, ViberMessageRequest):
-    message = viber_request.message
-    # lets echo back
-    viber.send_messages(viber_request.sender.id, [
-        message
-    ])
-  elif isinstance(viber_request, ViberSubscribedRequest):
-    viber.send_messages(viber_request.get_user.id, [
-        TextMessage(text="thanks for subscribing!")
-    ])
-  elif isinstance(viber_request, ViberFailedRequest):
-    logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+#   if isinstance(viber_request, ViberMessageRequest):
+#     message = viber_request.message
+#     # lets echo back
+#     viber.send_messages(viber_request.sender.id, [
+#         message
+#     ])
+#   elif isinstance(viber_request, ViberSubscribedRequest):
+#     viber.send_messages(viber_request.get_user.id, [
+#         TextMessage(text="thanks for subscribing!")
+#     ])
+#   elif isinstance(viber_request, ViberFailedRequest):
+#     logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 	
-  return Response(status=200)
+#   return Response(status=200)
 
-app.run(host = 'https://selecta-b2b-viber.herokuapp.com/', port = 443, debug = True)
+# app.run(host = 'https://selecta-b2b-viber.herokuapp.com/', port = 443, debug = True)
