80971bf09f1c863cb97d6aa672e6007555273af2
Francis Guevara <69579186+franciswguevara@users.noreply.github.com>
2021-08-31 09:37:50 +0000

    Removed Webhook code

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -24,24 +24,22 @@ viber = Api(BotConfiguration(
     auth_token= access_token
 ))
 
-viber.set_webhook('https://selecta-b2b-viber.herokuapp.com/')
+@app.route('/', methods=['POST'])
+def incoming():
+  # this library supplies a simple way to receive a request object
+  viber_request = viber.parse_request(request.get_data())
 
-# @app.route('/', methods=['POST'])
-# def incoming():
-#   # this library supplies a simple way to receive a request object
-#   viber_request = viber.parse_request(request.get_data())
+  if isinstance(viber_request, ViberMessageRequest):
+    # lets echo back
+    viber.send_messages(viber_request.sender.id, [
+        TextMessage(text="Your id is: " + str(viber_request.sender.id))
+    ])
+  elif isinstance(viber_request, ViberSubscribedRequest):
+    viber.send_messages(viber_request.get_user.id, [
+        TextMessage(text="Hvala što ste se prijavili na ovu uslugu!")
+    ])
 
-#   if isinstance(viber_request, ViberMessageRequest):
-#     # lets echo back
-#     viber.send_messages(viber_request.sender.id, [
-#         TextMessage(text="Your id is: " + str(viber_request.sender.id))
-#     ])
-#   elif isinstance(viber_request, ViberSubscribedRequest):
-#     viber.send_messages(viber_request.get_user.id, [
-#         TextMessage(text="Hvala što ste se prijavili na ovu uslugu!")
-#     ])
+  return Response(status=200)
 
-#   return Response(status=200)
-
-# if __name__ == "__main__":
-#   app.run(host='https://selecta-b2b-viber.herokuapp.com/', port=443, debug=True)
+if __name__ == "__main__":
+  app.run(host='https://selecta-b2b-viber.herokuapp.com/', port=443, debug=True)
