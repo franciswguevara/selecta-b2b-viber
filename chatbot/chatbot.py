de51bde15362125706fa4b60c7abcc8ae7a4b1c2
franciswguevara <69579186+franciswguevara@users.noreply.github.com>
2021-09-02 23:08:16 +0800

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -23,24 +23,29 @@ viber = Api(BotConfiguration(
     auth_token = access_token
 ))
 
-#viber.set_webhook('https://selecta-b2b-viber.herokuapp.com/')
-
 @app.route('/', methods=['POST'])
 def incoming():
-  # this library supplies a simple way to receive a request object
-  try:
-    viber_request = viber.parse_request(request.get_data())
+  
+  logger.debug("received request. post data: {0}".format(request.get_data()))
+  
+  # every viber message is signed, you can verify the signature using this method
+  if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+    return Response(status=403)
 
-    if isinstance(viber_request, ViberMessageRequest):
-      # lets echo back
-      viber.send_messages(viber_request.sender.id, [
-          TextMessage(text="Your id is: " + str(viber_request.sender.id))
-      ])
-    elif isinstance(viber_request, ViberSubscribedRequest):
-      viber.send_messages(viber_request.get_user.id, [
-          TextMessage(text="You're subscribed!")
-      ])
-    return Response(status=200)
-  except Exception as e:
-    print("THERE WAS AN ERROR NOOOO:",e)
-    return Response(status=200)
+  # this library supplies a simple way to receive a request object
+  viber_request = viber.parse_request(request.get_data())
+
+  if isinstance(viber_request, ViberMessageRequest):
+    message = viber_request.message
+    # lets echo back
+    viber.send_messages(viber_request.sender.id, [
+        message
+    ])
+  elif isinstance(viber_request, ViberSubscribedRequest):
+    viber.send_messages(viber_request.get_user.id, [
+        TextMessage(text="thanks for subscribing!")
+    ])
+  elif isinstance(viber_request, ViberFailedRequest):
+    logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+  return Response(status=200)
