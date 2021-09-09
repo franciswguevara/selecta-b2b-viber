5a4c6641b72bf0e4ae1ac66250f16f9a4e180791
franciswguevara <69579186+franciswguevara@users.noreply.github.com>
2021-09-10 00:38:03 +0800

    Modified Viberbot

---
+++ chatbot/viberbot/api/bot_configuration.py
@@ -0,0 +1,22 @@
+class BotConfiguration(object):
+	def __init__(self, auth_token, name, avatar, min_api_version=7):
+		self._auth_token = auth_token
+		self._name = name
+		self._avatar = avatar
+		self._min_api_version = min_api_version
+
+	@property
+	def name(self):
+		return self._name
+
+	@property
+	def avatar(self):
+		return self._avatar
+
+	@property
+	def auth_token(self):
+		return self._auth_token
+	
+	@property
+	def min_api_version(self):
+		return self._min_api_version
